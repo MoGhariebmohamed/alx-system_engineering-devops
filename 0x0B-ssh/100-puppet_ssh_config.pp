71c7fe86dfac870fb91794c9e30ace987384f9c9
mohamed gharieb <m.gharieb89@gmail.com>
2024-02-25 14:50:38 -0800

    t0100 v1

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+#using Puppet to make changes to our configuration file.
+exec { 'echo "PasswordAuthentication no\nIdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config' :
+	path => '/bin/'
+}
