d1edc0287c53bf9a6ec14e5b2d280732faa8672b
mohamed gharieb <m.gharieb89@gmail.com>
2024-03-25 19:22:52 -0700

    t1 v0

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -10,21 +10,18 @@ import requests
 import sys
 
 
-API_URL = "https://jsonplaceholder.typicode.com"
-
-
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-            employ_ID = int(sys.argv[1])
-            url_request = requests.get('{}/users/{}'
-                                       .format(API_URL, employ_ID)).json()
-            url_tasks = requests.get('{}/todos'.format(API_URL)).json()
-            usr_name = url_request.get('username')
-            tasks_stat = requests.get('{}/users/{}/todos'
-                                       .format(API_URL, employ_ID)).json()
-            with open('{}.csv'.format('employ_ID'), 'w') as csvfile:
-                for task in tasks_stat:
-                    done = task.get('complete')
-                    done_title = task.get('title')
-                    csvfile.write('"{}","{}","{}","{}"\n'.format(
-                                  employ_ID, usr_name, done, done_title))
+    API_URL = "https://jsonplaceholder.typicode.com"
+    employ_ID = sys.argv[1]
+    url_request = requests.get('{}/users/{}'
+                               .format(API_URL, employ_ID)).json()
+    url_tasks = requests.get('{}/todos'.format(API_URL)).json()
+    usr_name = url_request.get('username')
+    tasks_stat = requests.get('{}/users/{}/todos'
+                              .format(API_URL, employ_ID)).json()
+    with open('{}.csv'.format(employ_ID), 'w') as csvfile:
+        for task in tasks_stat:
+            done = task.get('completed')
+            done_title = task.get('title')
+            csvfile.write('"{}","{}","{}","{}"\n'.format(
+                          employ_ID, usr_name, done, done_title))
