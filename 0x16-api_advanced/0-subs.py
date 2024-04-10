c7c570a8785c58af58414219e3009a178dddd87b
mohamed gharieb <m.gharieb89@gmail.com>
2024-04-09 18:10:39 -0700

    t0v0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""queries the Reddit API and returns the number of subscribers """
+import json
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ function to return subscribers"""
+    user_agent = {"User-Agent": "unix:0-subs.py:v1.0"}
+    request_data = requests.get("https://www.reddit.com/r/{}/about.json"
+                 .format(subreddit), headers=user_agent, allow_redirects=False)
+    if (request_data.status_code != 200):
+        return 0
+    json_data = request_data.json()
+    final_results = json_data.get("data").get("subscribers")
+    return final_results
+
